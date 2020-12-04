4e7eb4905f8560c2c981fabad5c8d72f3438b707
masaun <masanoriuno@gmail.com>
2020-12-04 23:35:12 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Harvest.finance / Autostaking RewardPool for Multiple Assets
+
+***
+## 【Introduction of Autostaking RewardPool for Multiple Assets】
+- This is a smart contract for ...
+
+&nbsp;
+
+***
+
+## 【Workflow】
+
+&nbsp;
+
+***
+
+## 【Setup】
+### ① Install modules
+```
+$ npm install
+```
+
+<br>
+
+### ② Compile & migrate contracts (on Kovan testnet)
+```
+$ npm run migrate:kovan
+```
+
+<br>
+
+### ③ Execute script (it's instead of testing)
+```
+$ npm run script:kovan
+```
+
+
+&nbsp;
+
+***
+
+## 【References】
+- Harvest.finance  
+  - Prize：https://gitcoin.co/issue/harvest-finance/harvest/15/100024325  
+  - Doc：https://farm.chainwiki.dev/en/contracts  
+  - SC：https://github.com/harvest-finance/harvest  
+  - Addresses：https://github.com/harvest-finance/harvest#addresses  
---
+++ migrations/addressesList/contractAddress/contractAddress.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "Ropsten": {
+    },
+    "Kovan": {
+        "NexusMutual": {
+            NXMDSValueMock: [ '0x4C60Ca025972CC3F993244e8EBD3BA168708F2C3' ],
+            Claims: [ '0x47Ac8c8d43b1478e4277Bd6be42c0F527D2791ff' ],
+            ClaimsData: [ '0xbCcC739504D580fdc175617ede5E89c1E35Cab48' ],
+            ClaimsReward: [ '0xaEecC3Ca56A513EBe1Ea8fd38A3B887D0FAF5Ecc' ],
+            MCR: [ '0xfb238B174a35941a680ed9D327beED4A9e555BA5' ],
+            Pool1: [ '0x2E89c704665434e78bE20B59Ed84117F986c06Cc' ],
+            Pool2: [ '0x9FDF94Bef3CB7137e0e24F786E2937C267DBd3a0' ],
+            PoolData: [ '0xCca857cDa5673Af0b13F979BC929b2641a20ffef' ],
+            TokenData: [ '0xf6b23a78a9cc942D33Df9FEe60eaC70a3e625e07' ],
+            TokenFunctions: [ '0x599D5F6a905189c1Fd0C6fA732D75f97f8c3Da42' ],
+            Quotation: [ '0x493443a5ca842f20066aD0228b67d0A8e46CfFce' ],
+            QuotationData: [ '0x5d1a0CA3B2661C998B4938425680D4DE8A7d7FDf' ],
+            ClaimProofs: [ '0x8EAabbfbcB1722A0a1d7Fb4008c7b115496DFC2B' ],
+            OwnedUpgradeabilityProxy:
+               [ '0x32dd279456072290375e88c3196B297836840874',
+                 '0x348cAe942965A7e03E75A69733b446eB3Fc73c3a',
+                 '0xa14FCF2529F895B0d3b91C3a4c2d3ff95c54771f',
+                 '0x1a4d570E55efBFB90a6223d4A29C3c9c7E231C17',
+                 '0xc60ec9733F2F402870BeE3a40A3Ba0A6A73c8cd3',
+                 '0xEdC8f2D370b14c24C731db0448166F449566Fc48' ],
+            DisposableNXMaster: [ '0x525b1C10bD68b1B27662d828e5CEC96D6bEa5F79' ],
+            DisposableMemberRoles: [ '0xF445048301d85742D7b5b3AC35b4659ff63610B8' ],
+            DisposableTokenController: [ '0x59293aFb8c07471b13257873fd92B65Dd0396534' ],
+            DisposablePooledStaking: [ '0xA0B273FdD00fAd187248c07FE17AB67A82Da71c0' ],
+            DisposableProposalCategory: [ '0xD17c364995C07546DA9AA4D6FD3B6f8293A389c8' ],
+            DisposableGovernance: [ '0x2B307AABf76a43216683dC5Fc40Cd857c363b713' ],
+            NXMaster: [ '0x36d3c63a0030Dc172Ef8eb15D95C48DDF1374259' ],
+            MemberRoles: [ '0x5B3613A2d2FcDcB5c04b01377e1995d84DfaF099' ],
+            TokenController: [ '0x0723Eb765A5b4669e4bc5C792d1372259239C93f' ],
+            PooledStaking: [ '0xB6a14541eEB3dC9fEE06880399e99Ce83acb5905' ],
+            ProposalCategory: [ '0x5e967d422C28e540619833e5B5E915048bC427cA' ],
+            Governance: [ '0x15db425f2D085bd2aed7742b1B1b7a79fc7917Aa' ]
+        },
+        "Aave": {  /// Aave Market contracts
+            "LendingPoolAddressesProvider": "0x506B0B2CF20FAA8f38a4E2B524EE43e1f4458Cc5",
+            "LendingPool": "0x580D4Fdc4BF8f9b5ae2fb9225D584fED4AD5375c", 
+            "LendingPoolCore": "0x95D1189Ed88B380E319dF73fF00E479fcc4CFa45"
+        },
+    },
+}
