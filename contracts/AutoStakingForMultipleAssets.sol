0398f895244806d2d0c002bd53069396c51be9b8
masaun <masanoriuno@gmail.com>
2020-12-06 12:12:10 +0900

    Define methods from IMasterChef.sol on the SushiSwap

--- contracts/AutoStakingForMultipleAssets.sol
+++ contracts/AutoStakingForMultipleAssets.sol
@@ -7,6 +7,25 @@ pragma solidity ^0.5.16;
  */
 contract AutoStakingForMultipleAssets {
 
-  constructor() public {}
+    constructor() public {}
+
+    // Deposit LP tokens to MasterChef for SUSHI allocation.
+    function deposit(uint256 _pid, uint256 _amount) public;
+
+    // Withdraw LP tokens from MasterChef.
+    function withdraw(uint256 _pid, uint256 _amount) public;
+
+    // There are no definition in the MasterChef.sol on SushiSwap
+    function userInfo(uint256 _pid, address _user) public view returns (uint256 amount, uint256 rewardDebt);
+    function poolInfo(uint256 _pid) public view returns (address lpToken, uint256, uint256, uint256);
+    
+    // Update reward variables for all pools. Be careful of gas spending!
+    function massUpdatePools() public;
+    
+    // View function to see pending SUSHIs on frontend.
+    function pendingSushi(uint256 _pid, address _user) public view returns (uint256 amount);
+
+    // interface reused for pickle
+    function pendingPickle(uint256 _pid, address _user) public view returns (uint256 amount);
 
 }
