03723558eb15f97cff4b190b64022f56a1c16434
masaun <masanoriuno@gmail.com>
2020-12-06 12:01:06 +0900

    Rename FarmStakeAutomation.sol with AutoStakingForMultipleAssets.sol

---
+++ contracts/AutoStakingForMultipleAssets.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.16;
+
+/**
+ * @notice - The AutoStakingForMultipleAssets contract that automate fetching and staking $FARM rewards (that are earned by normal assets)
+ *         - Fetch $FARM rewards (by DAI, etc...) automatically.
+ *         - Stake these $FARM rewards into the profit sharing auto-staking pool automatically.
+ */
+contract AutoStakingForMultipleAssets {
+
+  constructor() public {}
+
+}
